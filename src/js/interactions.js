1202cbc6603002582e3f008e8ff549fa1acd2403
Sebestyen Orsolya <elfromka@gmail.com>
2022-09-12 20:08:33 +0300

    Uploading some more styling fixes and changes, changes in JS as well

--- src/js/interactions.js
+++ src/js/interactions.js
@@ -17,6 +17,72 @@ const dismissAlert = (e) => {
 };
 
 /**
+ * Called on sidenav related actions (open/close).
+ *
+ * @param {Event} e - data about the click action and about the clicked element itself
+ * @param {string} action - "open" is the default value, "close" can be set
+ *
+ * @return {boolean} - if the elements needed to make the action were found: true, else false
+ */
+const sideNavActions = (e, action = "open") => {
+    e.preventDefault();
+
+    const sideNavigationCloser = document.querySelector(
+        '[data-element-close="side-nav"]'
+    );
+    const sideNavigationElement = document.getElementById("sideNavigation");
+
+    if (!sideNavigationElement || !sideNavigationCloser) return false;
+
+    sideNavigationCloser.addEventListener(
+        "click",
+        () => {
+            sideNavActions(e, "close");
+        },
+        true
+    );
+
+    const documentBody = document.getElementById("root");
+    const backDrop = document.getElementById("backdrop");
+
+    if (action === "close") {
+        sideNavigationElement.style.removeProperty("width");
+        documentBody.classList.remove("overflow-hidden");
+        backDrop.classList.add("d-none");
+
+        sideNavigationCloser.removeEventListener("click", sideNavActions, true);
+
+        setTimeout(() => {
+            sideNavigationElement.firstElementChild.classList.add =
+                "visibility-hidden";
+            sideNavigationElement.firstElementChild.classList.add =
+                "opacity-low";
+
+            sideNavigationCloser.classList.add("visibility-hidden");
+            sideNavigationCloser.classList.add("opacity-low");
+        }, 250);
+
+        return true;
+    }
+
+    sideNavigationElement.style.width = "70vw";
+    documentBody.classList.add("overflow-hidden");
+    backDrop.classList.remove("d-none");
+
+    setTimeout(() => {
+        sideNavigationElement.firstElementChild.classList.remove(
+            "visibility-hidden"
+        );
+
+        sideNavigationElement.firstElementChild.classList.remove("opacity-low");
+        sideNavigationCloser.classList.remove("visibility-hidden");
+        sideNavigationCloser.classList.remove("opacity-low");
+    }, 250);
+
+    return true;
+};
+
+/**
  * Display modal with content.
  *
  * @summary - loads the modal with a certain type of content (here: language list, search). A "data-content-load" attribute MUST be set on the calle (button, etc.) which needs to be the same as the contents' components name.
@@ -28,6 +94,7 @@ const openModal = (e) => {
     const checkLoadableContent = e.currentTarget.dataset.contentLoad;
     const documentBody = document.getElementById("root");
     const modal = document.getElementById("modalWrapper");
+    const sideNavigation = document.getElementById("sideNavigation");
 
     (async () => {
         const loadModalContent = await loadHtml(
@@ -42,8 +109,18 @@ const openModal = (e) => {
             if (modalCloseButton) {
                 modalCloseButton.addEventListener("click", closeModal);
             }
+
+            if (checkLoadableContent === "search") {
+                const modalContent = document.getElementById("modalContent");
+
+                modalContent.classList.add("full-screen");
+            }
         }
     })();
+
+    if (sideNavigation && sideNavigation.style.width) {
+        sideNavActions(e, "close");
+    }
 
     documentBody.addEventListener(
         "click",
@@ -64,6 +141,11 @@ const closeModal = (e) => {
     const documentBody = document.getElementById("root");
     const modal = document.getElementById("modalWrapper");
 
+    const modalContent = document.getElementById("modalContent");
+    if (modalContent.classList.contains("full-screen")) {
+        modalContent.classList.remove("full-screen");
+    }
+
     removeHtml("modalContent");
 
     documentBody.removeEventListener(
@@ -77,7 +159,6 @@ const closeModal = (e) => {
 
 /**
  * Close modal when visitor clicks outside of it.
- * //TODO HOTFIX - re-check closing modal on click outside (event bubbling) - event listeners
  *
  * @param {string} elementId - the HTML elements' ID to get the that element which shouldn't be counted
  * as clicked outside (language-changer buttons and the modal itself without the backdrop)
@@ -85,8 +166,6 @@ const closeModal = (e) => {
  * @return {Function} - returns an object - currying
  */
 const closeWhenClickedOutside = (elementId) => {
-    // console.log("still here");
-
     return (e) => {
         // checks if the clicked element is from inside the modal
         const isFromModal = document
@@ -102,64 +181,6 @@ const closeWhenClickedOutside = (elementId) => {
         // modal opener buttons were found, close the modal
         if (!isFromModal && !isModalOpener) closeModal(e);
     };
-};
-
-const sideNavActions = (e, action = "open") => {
-    e.preventDefault();
-    const sideNavigationCloser = document.querySelector(
-        '[data-element-close="side-nav"]'
-    );
-    const sideNavigationElement = document.getElementById("sideNavigation");
-
-    if (!sideNavigationElement || !sideNavigationCloser) return false;
-
-    sideNavigationCloser.addEventListener(
-        "click",
-        () => {
-            sideNavActions(e, "close");
-        },
-        true
-    );
-
-    const documentBody = document.getElementById("root");
-    const backDrop = document.getElementById("backdrop");
-
-    if (action === "close") {
-        sideNavigationElement.style.width = "0";
-        documentBody.classList.remove("overflow-hidden");
-        backDrop.classList.add("d-none");
-
-        sideNavigationCloser.removeEventListener("click", sideNavActions, true);
-
-        setTimeout(() => {
-            sideNavigationElement.firstElementChild.classList.add =
-                "visibility-hidden";
-            sideNavigationElement.firstElementChild.classList.add =
-                "opacity-low";
-
-            sideNavigationCloser.classList.add("visibility-hidden");
-            sideNavigationCloser.classList.add("opacity-low");
-        }, 250);
-
-        return true;
-    }
-
-    // sideNavigationElement.style.width = "30rem";
-    sideNavigationElement.style.width = "70vw";
-    documentBody.classList.add("overflow-hidden");
-    backDrop.classList.remove("d-none");
-
-    setTimeout(() => {
-        sideNavigationElement.firstElementChild.classList.remove(
-            "visibility-hidden"
-        );
-
-        sideNavigationElement.firstElementChild.classList.remove("opacity-low");
-        sideNavigationCloser.classList.remove("visibility-hidden");
-        sideNavigationCloser.classList.remove("opacity-low");
-    }, 250);
-
-    return true;
 };
 
 /**
